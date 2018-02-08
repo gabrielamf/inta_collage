7dc55d1e2d06647b129f7c2232e8574792b86217
Jennifecarmen <jennifercarmens@gmail.com>
2018-02-08 05:32:54 -0500

    añadiendo funcion drop

--- js/app.js
+++ js/app.js
@@ -11,5 +11,12 @@ let begin = () => {
     ev.dataTransfer.setData('text', ev.target.id);
   }
 };
-
+// añadiendo funcion drop
+function drop(ev) {
+  event.preventDefault();
+  if (event.target.id === 'photo1' || event.target.id === 'photo2' || event.target.id === 'photo3' || event.target.id === 'photo4') {
+    var idFoto = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(idFoto));
+  }
+}
 window.addEventListener('load', begin);
